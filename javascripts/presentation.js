58f1ca30e25b5e62877597f43d90462725c38ead
Daniel Heath <daniel@danielhomepc.(none)>
2010-03-07 13:09:06 +1100

    getting closer to a working example

--- javascripts/presentation.js
+++ javascripts/presentation.js
@@ -1,2 +1,21 @@
-Equations = []
-Equations.multiply = function(value) { alert(value);};
+function Equations() {
+}
+
+Equations.multiply = function (value) { 
+  alert(value);
+};
+
+$(document).ready(function() {
+  $("#multiply_by_submit").click( function () {
+    Equations.multiply($('#multiply_by')[0].value);
+  })
+  
+  $.get('equation1.mml', function(data) {
+    $('#equations').append(
+      '<li class="equation" id="current_equation">' + 
+      data +
+      '</li>'
+    );
+  });
+});
+
