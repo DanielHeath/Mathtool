216a331e231341f6d94fd7e36b0f907265bae545
Daniel Heath <daniel@danielhomepc.(none)>
2010-03-07 13:37:07 +1100

    Working client-side only version

--- javascripts/presentation.js
+++ javascripts/presentation.js
@@ -1,8 +1,28 @@
 function Equations() {
 }
 
+Equations.line = function (inner) {
+  $('#current_equation').removeAttr("id");
+  
+  $('#equations').prepend(
+    '<li class="equation" id="current_equation">' + 
+    inner +
+    '</li>'
+  );
+};
+
+Equations.make_new_line = function () {
+  this.line($('#current_equation').html())
+};
+
+Equations.prepend_both_sides = function (string) {
+  this.make_new_line();
+  $('#current_equation .lhs').prepend(string);
+  $('#current_equation .rhs').prepend(string);
+};
+
 Equations.multiply = function (value) { 
-  alert(value);
+  this.prepend_both_sides('<mi>' + value + '</mi>');
 };
 
 $(document).ready(function() {
@@ -11,11 +31,7 @@ $(document).ready(function() {
   })
   
   $.get('equation1.mml', function(data) {
-    $('#equations').append(
-      '<li class="equation" id="current_equation">' + 
-      data +
-      '</li>'
-    );
+    Equations.line(data);
   });
 });
 
